f4e155330073e599ba6ff575b813168876bda06d
01635499 <45786985+01635499@users.noreply.github.com>
2018-12-17 15:07:00 +0800

    Update EkycEurekaServerApplication.java

--- src/main/java/com/hkt/ekyc/EkycEurekaServerApplication.java
+++ src/main/java/com/hkt/ekyc/EkycEurekaServerApplication.java
@@ -1,3 +1,5 @@
+pacakage com.hkt.ekyc;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
