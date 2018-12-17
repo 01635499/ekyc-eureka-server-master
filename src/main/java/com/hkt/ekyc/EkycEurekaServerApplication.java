503c8181de25ac15d864ed7ac5de550f727e5355
01635499 <45786985+01635499@users.noreply.github.com>
2018-12-17 14:23:48 +0800

    Update EkycEurekaServerApplication.java

--- src/main/java/com/hkt/ekyc/EkycEurekaServerApplication.java
+++ src/main/java/com/hkt/ekyc/EkycEurekaServerApplication.java
@@ -1,18 +1,17 @@
-package com.hkt.ekyc;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.boot.system.ApplicationPidFileWriter;
+import org.springframework.boot.context.ApplicationPidFileWriter;
+
 
 @EnableEurekaServer
 @SpringBootApplication
 public class EkycEurekaServerApplication {
 
 	public static void main(String[] args) {
-		 SpringApplication springApplication = new SpringApplication(EkycEurekaServerApplication.class);
-		springApplication.addListeners(new ApplicationPidFileWriter()); 
-		springApplication.run(args);
-	        
+        SpringApplication app = new SpringApplication(EkycEurekaServerApplication.class);
+        app.addListeners(new ApplicationPidFileWriter());
+        app.run(args);
+
 	}
 }
