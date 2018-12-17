5de35416eb4f4074cf7f46019e1e615622cd95d4
01635499 <45786985+01635499@users.noreply.github.com>
2018-12-17 14:53:30 +0800

    Update EkycEurekaServerApplicationTests.java

--- src/test/java/com/hkt/ekyc/EkycEurekaServerApplicationTests.java
+++ src/test/java/com/hkt/ekyc/EkycEurekaServerApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes=EkycEurekaServerApplication.class)
 public class EkycEurekaServerApplicationTests {
 
 	@Test
